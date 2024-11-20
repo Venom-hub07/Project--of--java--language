b6082e0ed942e0818a5c68b025d6b6bea8b56970
Raju VIshawakarma <rajuvish547@gmail.com>
2024-11-20 12:06:55 +0530

    Delete helloWorld/Hello World.c

--- helloWorld/Hello World.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-/**
- * This code is written in C programming language.
- * It contains a main function that prints "hello world" to the console.
- */
-    void main(){
-        printf("hello world");
-    }
